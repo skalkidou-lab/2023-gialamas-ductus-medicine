2e746f630a76d9c3ef2f18cf9810d4c161c43640
Richard Aubrey White <hello@rwhite.no>
2023-11-01 08:30:21 +0100

    Start

---
+++ Run.R
@@ -0,0 +1,14 @@
+# retrospective cohort study
+# swedish registries
+# extremely preterm baby born between 2007 and 2022
+# exposure was progressively changed
+# early, medicine was given, stopped using from 2015
+# binary exposure
+# 800 preterm children with ductus. 400 exposed, 400 not exposed.
+# outcome: bpd complications
+# 30% have it.
+# medicine does not have any effect
+# TOST (equality)
+# try to figure out how much will be the same
+#
+# doctus?
